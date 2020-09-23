a6cd90e967b22a109b73b90d3dfa70c733ac8fe9
John Stilia <stilia.johny@gmail.com>
2020-09-24 00:59:49 +0100

    edit rofi and alias

--- .config/polybar/scripts/vbox.sh
+++ .config/polybar/scripts/vbox.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
-echo "%{F#3cb703}   : $( vboxmanage list -s runningvms | wc -l) "
+if  command -v vboxmanage &> /dev/null
+then
+	echo "%{F#3cb703}   : $( vboxmanage list -s runningvms | wc -l) "
+elif ! command -v vboxmanage &> /dev/null
+then
+	echo -e "%{F#ff0000}   : N/A "
+fi
