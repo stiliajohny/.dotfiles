93cf06c1458eb4e63bdb1d6c82371e7405d2fb5e
John Stilia <stilia.johny@gmail.com>
2020-09-27 22:42:30 +0100

    edit rofi and alias

--- .config/polybar/scripts/vbox.sh
+++ .config/polybar/scripts/vbox.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 if  command -v vboxmanage &> /dev/null
 then
-	echo "%{F#3cb703}   : $( vboxmanage list -s runningvms | wc -l) "
+    case "$1" in
+    rofi-left) vboxmanage list -s runningvms | awk -F"{" {'print$1'}  | rofi -dmenu -window-title "Vbox Status"
+    		;;
+   	*) echo "%{F#3cb703}   : $( vboxmanage list -s runningvms | wc -l) "
+    		;;
+    	esac
 elif ! command -v vboxmanage &> /dev/null
 then
 	echo -e "%{F#ff0000}   : N/A "
