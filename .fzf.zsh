b56f7b370e1c131912bd162d446c1d8ea664659f
John Stilia <stilia.johny@gmail.com>
2020-10-07 13:50:16 +0100

    update vimrc with coc and plugins , update fzf with relative path, updae tmux colour on select

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/jstilia/.config/oh-my-zsh/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/jstilia/.config/oh-my-zsh/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.config/oh-my-zsh/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.config/oh-my-zsh/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/jstilia/.config/oh-my-zsh/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.config/oh-my-zsh/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/jstilia/.config/oh-my-zsh/fzf/shell/key-bindings.zsh"
+source "$HOME/.config/oh-my-zsh/fzf/shell/key-bindings.zsh"
