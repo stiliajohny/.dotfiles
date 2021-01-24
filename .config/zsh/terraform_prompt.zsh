43e571ffa84b5f89c2db9c3a72a5a60c2c4872c8
John Stilia <stilia.johny@gmail.com>
2021-01-24 22:09:53 +0000

    update zshrc and source files or the rc

--- .config/zsh/terraform_prompt.zsh
+++ .config/zsh/terraform_prompt.zsh
@@ -2,6 +2,6 @@ zsh_terraform() {
   # break if there is no .terraform directory
   if [[ -d .terraform ]]; then
       local tf_workspace=$($(which terraform) workspace show)
-      echo -n "(TF: $tf_workspace)-"
+      echo -n "(%{%F{4}%}%{%f%}|%{%F{1}%}TF%{%f%}:%{%F{6}%}$tf_workspace%{%f%})-"
   fi
 }
